fb84d21babede08f6436ab794fb32a6884dd2910
dylanhwcho <dylanhwcho@gmail.com>
2024-12-27 14:34:25 +0900

    logo added

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:phone_demo/main.dart';
+import 'package:phone_demo/contacts.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
