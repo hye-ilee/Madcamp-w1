996c9d27b1354e6b60c70ea581a1123278c41bb0
hye-ilee <harriet@kaist.ac.kr>
2024-12-30 11:11:18 +0900

    popup helper

--- lib/cafe_list.dart
+++ lib/cafe_list.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:phone_demo/helpers/database_helper.dart';
+import 'package:phone_demo/helpers/popup_helper.dart';
 
 class CafeListScreen extends StatefulWidget {
   const CafeListScreen({Key? key}) : super(key: key);
@@ -31,33 +32,7 @@ class _CafeListScreenState extends State<CafeListScreen> {
     //   _resultsPopup(results);
     // }
   }
-  
-  void _cafeInfoPopup(Map<String, dynamic> cafe) {
-    showDialog(
-      context: context,
-      builder: (context) {
-        return AlertDialog(
-          title: Text(cafe['name']),
-          content: Column(
-            mainAxisSize: MainAxisSize.min,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text('메뉴: ${cafe['menus']}'),
-              Text('연락처: ${cafe['phone']}'),
-              Text('위치: ${cafe['location']}'),
-            ],
-          ),
-          actions: [
-            TextButton(onPressed: () {
-              Navigator.of(context).pop();
-            },
-                child: const Text('close')
-            ),
-          ],
-        );
-      },
-    );
-  }
+
   final List<Map<String, String>> categories = [
     {'image': 'assets/music.png', 'label': '음악이 좋은'},
     {'image': 'assets/study.png', 'label': '공부하기 좋은'},
@@ -152,109 +127,121 @@ class _CafeListScreenState extends State<CafeListScreen> {
           ),
           Expanded(
             child: _isSearching ?
-              ListView.builder(
+            (_searchResults.isEmpty ?
+              const Center(
+                child: Text('검색 결과가 없습니다',
+                  style: TextStyle(
+                    fontSize: 18.0,
+                    color: Colors.grey,
+                  ),
+                ),
+              )
+              : ListView.builder(
                 itemCount: _searchResults.length,
                 itemBuilder: (context, index) {
                   final cafe = _searchResults[index];
                   return ListTile(
                     title: Text(cafe['name']),
                     onTap: () {
-                      _cafeInfoPopup(cafe);
+                      showCafeInfoPopup(context, cafe);
                     },
                   );
                 },
-              )
-              : Column(
-                  children: [
-                  GridView.builder(
-                    padding: const EdgeInsets.all(8.0),
-                    shrinkWrap: true,
-                    physics: const NeverScrollableScrollPhysics(),
-                    gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                      crossAxisCount: 3,
-                      crossAxisSpacing: 8.0,
-                      mainAxisSpacing: 8.0,
-                    ),
-                    itemCount: categories.length,
-                    itemBuilder: (context, index) {
-                      final category = categories[index];
-                      return GestureDetector(
-                        onTap: () async {
-                          final fieldName =
-                            _getFieldNameByCategoryLabel(category['label']!);
-                          if (fieldName != null) {
-                            await DatabaseHelper.instance.incrementUserCount(
-                              '${fieldName}_cnt'); // Increment user's category count
+              ))
+            : Column(
+                children: [
+                GridView.builder(
+                  padding: const EdgeInsets.all(8.0),
+                  shrinkWrap: true,
+                  physics: const NeverScrollableScrollPhysics(),
+                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 3,
+                    crossAxisSpacing: 8.0,
+                    mainAxisSpacing: 8.0,
+                  ),
+                  itemCount: categories.length,
+                  itemBuilder: (context, index) {
+                    final category = categories[index];
+                    return GestureDetector(
+                      onTap: () async {
+                        final fieldName =
+                          _getFieldNameByCategoryLabel(category['label']!);
+                        if (fieldName != null) {
+                          await DatabaseHelper.instance.incrementUserCount(
+                            '${fieldName}_cnt'); // Increment user's category count
 
-                            final userInfo = await DatabaseHelper.instance.fetchUserInfo(1); // Ensure user info exists
-                            if (userInfo != null) {
-                              final userCntField = '${fieldName}_cnt';
-                              final userCnt = userInfo[userCntField] ?? 1; // Default to 1 if null
+                          final userInfo = await DatabaseHelper.instance.fetchUserInfo(1); // Ensure user info exists
+                          if (userInfo != null) {
+                            final userCntField = '${fieldName}_cnt';
+                            final userCnt = userInfo[userCntField] ?? 1; // Default to 1 if null
 
-                              setState(() {
-                                _topCafes = DatabaseHelper.instance.getTopCafesByCategory(fieldName, userCnt);
-                              });
+                            setState(() {
+                              _topCafes = DatabaseHelper.instance.getTopCafesByCategory(fieldName, userCnt);
+                            });
 
-                              ScaffoldMessenger.of(context).showSnackBar(
-                                SnackBar(
-                                  content: Text('${category['label']} 기준으로 카페를 정렬합니다.')
-                                ),
-                              );
-                            }
-                          } else {
                             ScaffoldMessenger.of(context).showSnackBar(
-                              SnackBar(content: Text('잘못된 카테고리 선택입니다.')),
+                              SnackBar(
+                                content: Text('${category['label']} 기준으로 카페를 정렬합니다.')
+                              ),
                             );
                           }
-                        },
-                        child: Column(
-                          children: [
-                            Expanded(
-                              child: Image.asset(
-                                category['image']!,
-                                fit: BoxFit.contain,
-                              ),
+                        } else {
+                          ScaffoldMessenger.of(context).showSnackBar(
+                            SnackBar(content: Text('잘못된 카테고리 선택입니다.')),
+                          );
+                        }
+                      },
+                      child: Column(
+                        children: [
+                          Expanded(
+                            child: Image.asset(
+                              category['image']!,
+                              fit: BoxFit.contain,
                             ),
-                            const SizedBox(height: 8.0),
-                            Text(
-                              category['label']!,
-                              textAlign: TextAlign.center,
-                              style: const TextStyle(fontSize: 14.0),
-                            ),
-                          ],
-                        ),
-                      );
-                    },
-                  ),
-                  const SizedBox(height: 16.0),
-                  FutureBuilder<List<Map<String, dynamic>>>(
-                    future: _topCafes,
-                    builder: (context, snapshot) {
-                      if (snapshot.connectionState == ConnectionState.waiting) {
-                        return const Center(child: CircularProgressIndicator());
-                      } else if (snapshot.hasError) {
-                        return Center(child: Text('Error: ${snapshot.error}'));
-                      } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
-                        return const Center(child: Text('No cafes found.'));
-                      } else {
-                        final cafes = snapshot.data!;
-                        return ListView.builder(
-                          shrinkWrap: true,
-                          physics: const NeverScrollableScrollPhysics(),
-                          itemCount: cafes.length,
-                          itemBuilder: (context, index) {
-                            final cafe = cafes[index];
-                            return ListTile(
+                          ),
+                          const SizedBox(height: 8.0),
+                          Text(
+                            category['label']!,
+                            textAlign: TextAlign.center,
+                            style: const TextStyle(fontSize: 14.0),
+                          ),
+                        ],
+                      ),
+                    );
+                  },
+                ),
+                const SizedBox(height: 16.0),
+                FutureBuilder<List<Map<String, dynamic>>>(
+                  future: _topCafes,
+                  builder: (context, snapshot) {
+                    if (snapshot.connectionState == ConnectionState.waiting) {
+                      return const Center(child: CircularProgressIndicator());
+                    } else if (snapshot.hasError) {
+                      return Center(child: Text('Error: ${snapshot.error}'));
+                    } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
+                      return const Center(child: Text('No cafes found.'));
+                    } else {
+                      final cafes = snapshot.data!;
+                      return ListView.builder(
+                        shrinkWrap: true,
+                        physics: const NeverScrollableScrollPhysics(),
+                        itemCount: cafes.length,
+                        itemBuilder: (context, index) {
+                          final cafe = cafes[index];
+                          return ListTile(
                             title: Text(cafe['name']),
                             trailing: Text('Location: ${cafe['location']}'),
-                            );
-                          },
-                        );
-                      }
-                    },
-                  ),
-                ],
-              ),
+                            onTap: () {
+                              showCafeInfoPopup(context, cafe);
+                            },
+                          );
+                        },
+                      );
+                    }
+                  },
+                ),
+              ],
+            ),
           ),
         ],
       ),
